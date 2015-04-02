91d5dab2a6696fc38a2d578a361cfc6c2afbb44f
Nicolas Fischer <nicolas.fischer@netapsys.fr>
2015-04-02 09:56:49 +0200

    Squelette applicatif - bootstrap game.js

---
+++ js/game.js
@@ -0,0 +1,24 @@
+/**
+ * Game launcher
+ */
+
+var gameWidth = 70 * 16;
+var gameHeight = 70 * 10;
+
+var game = new Phaser.Game(
+    gameWidth,
+    gameHeight,
+    Phaser.CANVAS,
+    'phaser-example'
+);
+
+/** Define all the states */
+// State 1 - Game initialisation
+game.state.add('load', loadState);
+// State 2 - Game menu
+game.state.add('menu', menuState);
+// State 3 - Let's play !
+game.state.add('play', playState);
+
+// Start with the 'load' state
+game.state.start('load');
