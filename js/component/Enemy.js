b8bc1a15cfac7e1f789267f01d8d952e57560f87
Nicolas Fischer <nicolas.fischer@netapsys.fr>
2015-04-06 08:43:30 +0200

    Déplacement intelligent des ennemis

--- js/component/Enemy.js
+++ js/component/Enemy.js
@@ -4,6 +4,7 @@ Enemy = function(game) {
     this.image = 'assets/potatoe.png';
     this.sprite = null;
     this.enemies = null;
+    this.velocity = 50;
 
 };
 
@@ -42,29 +43,35 @@ Enemy.prototype = {
     _createEnemy: function () {
 
         // Random position
-        positionX = Math.floor(Math.random() * (gameWidth - 40));
+        positionX = Math.floor(Math.random() * (gameWidth));
+        positionY = Math.floor(Math.random() * (gameHeight));
+
+        positionX = (positionX < 80) ? 80 : positionX;
+        positionX = (positionX > (gameWidth - 80)) ? gameWidth - 80 : positionX;
+
+        positionY = (positionY < 80) ? 80 : positionY;
+        positionY = (positionY > (gameHeight - 80)) ? gameHeight - 80 : positionY;
+
 
         //  Create a enemie inside of the 'enemies' group
-        var enemy = this.enemies.create(positionX, 0, 'enemy1');
+        var enemy = this.enemies.create(positionX, positionY, 'enemy1');
         enemy.scale.setTo(0.38, 0.38);
 
         // Set the anchor point centered at the bottom
         enemy.anchor.setTo(0.5, 1);
-        // Put the enemy above the top hole
-        enemy.reset(positionX, positionX);
         // Add gravity to see it fall
         //enemy.body.gravity.y = 500;
 
         // We give some horizontal velocity to the enemy to make it move right or left. We use Phaser.Math.randomSign()
         // that will randomly return 1 or -1, to have a velocity of 100 or -100:
-        enemy.body.velocity.x = 100 * Phaser.Math.randomSign();
+        //enemy.body.velocity.x = 100 * Phaser.Math.randomSign();
 
         // When an enemy is moving right and hits a wall, we want it to start moving left. One easy way
         // to do this is to use the bounce property that can be set with a value between 0 and 1. 0 means no
         // bounce, 1 means a perfect bounce. So this will make the enemy change direction when hitting a wall
         // horizontally:
-        enemy.body.bounce.x = 1;
-        enemy.body.bounce.y = 1;
+        /*enemy.body.bounce.x = 1;
+        enemy.body.bounce.y = 1;*/
 
         // And finally, these 2 lines will automatically kill the sprite when it’s no longer in the world (when it
         // falls into the bottom hole). This way we should never run out of dead enemies for getFirstDead .
@@ -74,12 +81,20 @@ Enemy.prototype = {
 
     update: function() {
 
-        // Reset the velocity (movement)
-        //this.sprite.body.velocity.x = 0;
+        this.enemies.forEach(function(item) {
+            if (item.x < game.player.sprite.x) {
+                item.body.velocity.x = this.velocity;
+            } else if (item.x > game.player.sprite.x) {
+                item.body.velocity.x = this.velocity * -1;
+            }
 
-        // Move to the right
-        //this.sprite.body.velocity.x = 100;
-        //this.sprite.animations.play('right');
+            if (item.y < game.player.sprite.y) {
+                item.body.velocity.y = this.velocity;
+            } else if (item.y > game.player.sprite.y) {
+                item.body.velocity.y = this.velocity * -1;
+            }
+
+        }, this);
 
     },
 
