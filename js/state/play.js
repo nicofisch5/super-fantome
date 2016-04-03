aecef2a907c3e7f2cd1b48683424234f6e580b55
Nicolas Fischer <nicolas.fischer@netapsys.fr>
2016-04-03 07:21:30 +0200

    [+] Add sound when player dies

--- js/state/play.js
+++ js/state/play.js
@@ -192,6 +192,8 @@ playState.prototype = {
         this.emitter.y = this.player.sprite.y;
         this.emitter.start(true, 600, null, 15);
 
+        this.player.playSound();
+
     },
 
     /**
@@ -316,6 +318,8 @@ playState.prototype = {
      */
     _goToNextLevel: function () {
 
+        this.backgroundSound.stop();
+        
         this.game.score += this.level.timer;
 
         this.game.levelNumber++;
